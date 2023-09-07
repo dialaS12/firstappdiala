6e030c634aa115214383d1731756a06315596eda
diala <dialagadban5@gmail.com>
2023-09-07 13:58:54 +0300

    d bhvhvg 1.bv vcc 2. gcgcgh

--- app/src/main/java/com/example/firstappdiala/MainActivity.java
+++ app/src/main/java/com/example/firstappdiala/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //jjbjhvhvhvhvhvhvvhvhvhvhvhhvvhhvhvhvvhvhvhvhvhvhvhvhvhvhvhvhvhvhhvvhvhhvvhvhhvvhvhvvhvvvvvhvvgvvgvgv
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
